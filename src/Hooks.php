fffd12e263d291f17747df4db3d0696c88243830
alex4401 <rylatgl@gmail.com>
2024-05-31 00:18:31 +0200

    feat: enlist Special:NewRedirect in the toolbox

--- src/Hooks.php
+++ src/Hooks.php
@@ -28,7 +28,35 @@ final class Hooks implements
 						'text' => $skin->msg( 'extnewpage-toolbox-label' )->text(),
 						'accesskey' => Linker::accesskey( 'extnewpage' ),
 					],
-				]
+				],
+			);
+		}
+
+		if ( !$skin->msg( 'extnewpage-redirect-toolbox-label' )->inContentLanguage()->isDisabled() ) {
+			$query = [];
+			// If this is not a virtual page, we can probably prefill the form fields.
+			//
+			// - If the request is for a missing page, prefill the title as the redirect title.
+			// - If the request is for a page already created, prefill the title as the redirect target.
+			if ( $skin->getRelevantTitle()->canExist() ) {
+				$query[$skin->getRelevantTitle()->exists() ? 'nrto' : 'nrfrom'] =
+					$skin->getRelevantTitle()->getFullText();
+			}
+
+			$sidebar['TOOLBOX'] = self::mergeBefore(
+				$sidebar['TOOLBOX'],
+				[
+					'extnewpage',
+					'upload',
+					'specialpages',
+				],
+				[
+					'extnewredirect' => [
+						'id' => 't-newredirect',
+						'href' => SpecialPage::getTitleFor( 'NewRedirect' )->getLocalURL( $query ),
+						'text' => $skin->msg( 'extnewpage-redirect-toolbox-label' )->text(),
+					],
+				],
 			);
 		}
 	}
